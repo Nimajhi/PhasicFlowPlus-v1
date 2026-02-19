6c7692f89de64aa9540fad0643c301060b0029af
Nima Joghataei <nimajoghataii@gmail.com>
2026-02-19 14:09:50 +0330

    Update Cello.hpp
    minor correction on coefficients

--- phasicFlowCoupling/couplingSystem/unresolved/interaction/drag/dragClosures/Cello.hpp
+++ phasicFlowCoupling/couplingSystem/unresolved/interaction/drag/dragClosures/Cello.hpp
@@ -48,27 +48,26 @@ public:
         const Foam::scalar epc = Foam::max(ep, Foam::SMALL);
         const Foam::scalar ep2 = epc*epc;
         const Foam::scalar ep3 = ep2*epc;
-        const Foam::scalar ep4 = ep2*ep2;
+       
 
         const Foam::scalar K0 = (1.0 - epc)/(1.0 + 3.0*epc);
         const Foam::scalar K1 = (1.0 + 128.0*K0 + 715.0*Foam::sqr(K0))
             /(ep2*(1.0 + 49.5*K0));
 
         const Foam::scalar Re2 = Rec*Rec;
-        const Foam::scalar Re4 = Re2*Re2;
 
         const Foam::scalar K2 =
             (1.0 + 0.130*Rec + 6.66e-4*Re2)
             /(1.0 + 3.42e-2*Rec + 6.92e-6*Re2);
 
-        const Foam::scalar K3n = -410.0*epc + 9.20e-7*Rec*Foam::sqr(K0)
-            + 1900.0*ep3 - 6.60e-2*Re4;
+        const Foam::scalar K3n = -410.0*epc + 9.20e7*Rec*Foam::pow(K0,20)
+            + 1900.0*ep2 - 6.60e-2*Rec;
 
-        const Foam::scalar K3d = 6600.0*epc + 4.92e-4*Re4 - 4.30e4*ep2
+        const Foam::scalar K3d = 6600.0*epc + 4.92e-4*Re - 4.30e4*ep2
             - 1.31e-4*Re2 + 7.38e4*ep3;
 
         const Foam::scalar K3 =
-            (2.0*Re2/(1.0 + Re2))
+            (2.0*Re2/(1.0 + Rec))
             *(K3n/Foam::max(K3d, Foam::SMALL));
 
         return K1 + K2*ep4 + K3*(1.0 - ep4);
